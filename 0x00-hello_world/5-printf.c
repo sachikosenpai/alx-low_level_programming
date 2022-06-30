c5cf58e3d095fe472ae72c34963067d0f3e7b24c
sachikosenpai <popoolafeyisayo5@gmail.com>
2022-06-30 09:15:00 -0700

    print

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 
 /**
- * main - initial point
- *
- * return: (0) (code successful)
- */
+* main - initial point
+*
+* return: (0) (code successful)
+*/
 int main(void)
 {
  	   printf("with proper grammar, but the outcome is a piece of art,\n");
