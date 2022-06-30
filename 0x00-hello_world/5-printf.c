084675d6f22e169fa9fbdcb8f91541d0c24c2604
sachikosenpai <popoolafeyisayo5@gmail.com>
2022-06-30 09:45:22 -0700

    print

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -2,7 +2,7 @@
 
 
 /**
- * main - initial point
+ * main- initial point
  *    
  * Return: 0 (Successful)
  */
@@ -12,8 +12,7 @@ int main(void)
 {
 
 
-	
-printf("with proper grammar, but the outcome is a piece of art,\n");
+	printf("with proper grammar, but the outcome is a piece of art,\n");
 
 			return (0);
 
