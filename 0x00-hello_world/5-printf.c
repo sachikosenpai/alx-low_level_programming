46e180b856adc650427caf9e5851ad21cf451e36
sachikosenpai <popoolafeyisayo5@gmail.com>
2022-06-30 09:34:44 -0700

    print

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -3,8 +3,7 @@
 
 /**
  * main - initial point
- *
- *  
+ *    
  *
  * Return: 0 (Successful)
  *
@@ -19,5 +18,6 @@ int main(void)
 printf("with proper grammar, but the outcome is a piece of art,\n");
 
 			return (0);
+
 
 }
