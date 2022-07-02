6e1dc61fc4a5e5eaf61462d2cc5650e35159322c
sachikosenpai <popoolafeyisayo5@gmail.com>
2022-07-02 15:38:17 -0700

    last digit

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -27,19 +27,19 @@ char last[] = "Last digit of";
 				printf("%s %d is %d and is ", last, n, n % 10);
 
                                 if (n % 10 > 5)
-                               {
+{
                                          printf("greater than 5\n");
-                               }
+}
 
                                else if (n % 10 == 0)
 
-                               { 
+{ 
                                             printf("0\n");
-			       }	    
+}	    
                                else
-			       {       
+{       
 		  	                     printf("less than 6 and not 0\n");
-			       }	     
+}	     
 
 					return (0);
 
