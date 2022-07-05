a01c9aa761dc547d9ff0d6c3ad0658d22026fc85
sachikosenpai <popoolafeyisayo5@gmail.com>
2022-07-05 12:05:09 -0700

    lower

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -2,7 +2,7 @@
 
 /**
  * _islower - checks if a character is lowercase.
-	* @c: The character to be checked 
+ * @c: The character to be checked 
  *
  * Return: 1 if character is lowercase, 0othrwise.
  */
