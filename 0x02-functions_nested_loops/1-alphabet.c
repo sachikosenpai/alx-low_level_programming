5e3db671441bfebf642cbca9a7e9261c68f831ce
sachikosenpai <popoolafeyisayo5@gmail.com>
2022-07-05 15:42:43 -0700

    1-alpha

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -8,10 +8,12 @@
 
 void print_alphabet(void)
 {
-	char alphabet;
+	char c;
 
-		for ( alphabet = 'a'; alphabet<= 'z'; alphabet++)
-			_putchar(alphabet);
+		for ( c = 'a'; c <= 'z'; c++)
+		{
+			_putchar(c);
+}
 		
 			_putchar('\n');
 }
