3042b313cfbe8329a0d73251af7b6c16d47d7c54
sachikosenpai <popoolafeyisayo5@gmail.com>
2022-07-05 13:22:16 -0700

    sign

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -2,11 +2,11 @@
 
 /**
  * print_sign - prints the sign of a number
- * @n: the number pf which the sign will be printed 
+ * @n: the number of which the sign will be printed
  *
  * Return: if nis greater than 0
  * 	0 if n is zero
- * 	-1 is n is less tha 0
+ * 	-1 is n is less than 0
  */
 
 int print_sign(int n)
@@ -22,7 +22,7 @@ int print_sign(int n)
 		return (0);
 	}
 	else
-	{		
+	{
 		_putchar('-');
 		return (-1);
 	}
