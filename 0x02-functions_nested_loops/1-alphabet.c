b96ec87c00b21ca238163bf815c3e6d239fe8eb0
sachikosenpai <popoolafeyisayo5@gmail.com>
2022-07-05 09:01:19 -0700

    lower alphabets

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -8,8 +8,9 @@
 
 void print_alphabet(void)
 {
-	char alphabet
-		for ( alphabet = 'a'; letter<= 'z'; alphabet++)
+	char alphabet;
+
+		for ( alphabet = 'a'; alphabet<= 'z'; alphabet++)
 			_putchar(letter);
 
 	putchar('\n');
