3802b813b31b1a37054b6b53a7603e83ea421302
sachikosenpai <popoolafeyisayo5@gmail.com>
2022-07-05 10:06:01 -0700

    lower case

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,23 @@
+
+
+#include "main.h"
+
+/**
+ * main print lower case
+ *
+ * return 1 if c is lower case and 0 otherwise
+ */
+
+int _islower(int c)
+
+{
+
+		if (c >= 'a' && c <= 'z')
+
+					return (1);
+
+			else
+
+						return (0);
+
+}
