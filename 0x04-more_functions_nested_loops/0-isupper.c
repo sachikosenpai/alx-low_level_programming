3bddebd57ccfbad2bb684ac9b5649d61b5f4e775
sachikosenpai <popoolafeyisayo5@gmail.com>
2022-07-07 02:16:12 -0700

    check for upper case

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _isupper - checks for upper case chracter
+ * @c: character to be checked
+ *
+ * Return: 1 if c is uppercase otherwise 0
+ */
+
+int _isupper(int c)
+{
+	char c;
+	
+	if ( c == 'A')
+	{
+	return (1);
+	}
+
+	else
+		return (0);
+}
---
+++ 0x04-more_functions_nested_loops/main.h
@@ -0,0 +1,12 @@
+#ifndef main_h
+#define main_h
+
+/**
+ * file - main.h
+ *
+ * header file for this directory
+ */
+
+int _isupper(int c);
+
+#endif
