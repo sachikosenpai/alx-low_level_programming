6e35d3d3d8bf325b78d876ff246c51c955062a6a
sachikosenpai <popoolafeyisayo5@gmail.com>
2022-07-07 02:50:34 -0700

    positive and negative

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * main - to test if number is positive or negative
+ *
+ * Return: - Always(0)
+ */
+
+
+
+int main(void)
+
+{
+
+	        int i;
+
+
+
+		        i = 98;
+
+			        positive_or_negative(i);
+
+
+
+				        return (0);
+
+}
