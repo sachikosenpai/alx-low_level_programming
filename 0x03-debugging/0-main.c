7cf8f3e3d50edf492c7c3e740d0d3d78f7292570
sachikosenpai <popoolafeyisayo5@gmail.com>
2022-07-07 03:43:36 -0700

    positive or negative

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/** 
+ * positive_or_negative - to test the function
+ *
+ * Return: Always(0)
+ */
+
+int main(void)
+{
+	int i;
+
+	i = 98;
+	_putchar( positive_or_negative (i));
+
+	return (0)
+
+}
