af7ab997bc7c443738fa7601eacd56e3ca2bddef
sachikosenpai <popoolafeyisayo5@gmail.com>
2022-07-08 14:10:41 -0700

    print line

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -2,7 +2,7 @@
 
 /**
  * print_line - a function that draws a straight line in the terminal.
- * @c: charater 
+ * @n: charater 
  *
  * Return: n if 0 is less print \n
  */
@@ -11,7 +11,8 @@ void print_line(int n)
 {
 	int i;
 
-	if (n >= 0;)
+	if (n >= 0);
 		for (i = 0; i < n ; i++)
 			_putchar('_');
 			_putchar('\n');
+}
