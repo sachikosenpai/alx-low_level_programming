103f3953a7d4eb5c75faf3633a099e437024b47a
sachikosenpai <popoolafeyisayo5@gmail.com>
2022-07-08 12:16:30 -0700

    digit

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+#include <stdio.h>
+
+/**
+ * main - function that checks for a digit (0 through 9)
+ *
+ * Return - Always 0
+ */
+
+int main(void)
+{
+	if (c >= '0' && c <= '9')
+		return 1;
+	else 
+		return 0;
+}
