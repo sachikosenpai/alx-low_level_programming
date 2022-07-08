ee211399e6deb8da35a41a80ac5aba946e757d7c
sachikosenpai <popoolafeyisayo5@gmail.com>
2022-07-08 13:02:06 -0700

    print numbers

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_numbers - to print numbers from 0 - 9
+ *
+ * Return: Always (0)
+ */
+
+void print_numbers(void)
+{
+	int i;
+	for (i = 0; i <= 9; i++)
+	{
+		_putchar(i);
+	}
+	_putchar('\n'); 
+}
