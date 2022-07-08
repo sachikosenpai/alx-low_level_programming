b4505deb16b1bce3e2075e0f8dd3d40522916707
sachikosenpai <popoolafeyisayo5@gmail.com>
2022-07-08 16:34:52 -0700

    print

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -2,7 +2,7 @@
 
 /**
  * print_number - function that prints an integer
- * @n: integer 
+ * @n: integer
  *
  */
 
@@ -10,14 +10,14 @@ void print_number(int n)
 {
 	unsigned int num = n;
 
-	if(n < 0)
+	if (n < 0)
 	{
 		_putchar('-');
 		num = -num;
 	}
 
 	if ((num / 10 > 0)
-			print_number(num /10);
+			print_number(num / 10);
 
 			_putchar((num % 10) + 0);
 	}
