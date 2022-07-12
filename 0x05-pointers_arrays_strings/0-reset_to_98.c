a758e4b492cb4dd7b776fad009a25d01b56eaa04
sachikosenpai <popoolafeyisayo5@gmail.com>
2022-07-12 04:46:34 -0700

    arrays and pointers

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -11,16 +11,16 @@
 
 void reset_to_98(int *n)
 {
-	int n;
-	int *p;
+	int g;
+	int *n;
 
-	n = 402;
-	p = &n;
+	g = 402;
+	n = &g;
 
+	printf("Value of 'g': %d\n", g);
+	printf("Address of 'g': %p\n", &g);
+	printf("Value of 'n': %p\n", n);
+	*n = 98;
 	printf("Value of 'n': %d\n", n);
-	printf("Address of 'n': %p\n", &n);
-	printf("Value of 'p': %p\n", p);
-	*p = 98;
-	printf("Value of 'p': %d\n", p);
-	return (0);
+	
 }
