1620b5b1bf55b1c84ee311c04b2234acff25a885
sachikosenpai <popoolafeyisayo5@gmail.com>
2022-07-12 08:58:19 -0700

    arrays and pointers

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -3,9 +3,9 @@
 
 /**
  * print_array -  a function that prints n elements of an array of integers
- * @*a - first integer
- * @n - second integer
- *
+ * @a: first integer
+ * @n: second integer
+ * 
  */
 
 void print_array(int *a, int n)
