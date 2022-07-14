29377014263946bb8e4a20f84f94eb42f919132b
sachikosenpai <popoolafeyisayo5@gmail.com>
2022-07-14 04:23:19 -0700

    strcat.c

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <string.h>
+/**
+ * _strcat - function that concatenates two strings
+ * @*dest: first string
+ * @src: second string
+ * 
+ * Return - nothing
+ */
+
+char *_strcat(char *dest, char *src)
+{
+	char destination[] = "dest ";
+	char source [] = "src";
+	_strcat(destination,source);
+	_putchar(%s, destination):
+
+		_putchar('\n');
+}
---
+++ 0x06-pointers_arrays_strings/main.h
@@ -0,0 +1,11 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/**
+ * header for this repository
+ *
+ */
+char *_strcat(char *dest, char *src);
+int _putchar(char c);
+
+#endif
