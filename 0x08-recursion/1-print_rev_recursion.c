5d8fcd5f423e7b44c853c330ea21e3dd70bd28ac
sachikosenpai <popoolafeyisayo5@gmail.com>
2022-07-21 04:58:43 -0700

    recursion

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -7,9 +7,10 @@
  */
 void _print_rev_recursion(char *s)
 {
-	if s[0] == '\0'
+	if (s[0] == '\0')
 		return (0);
-	if s[1] != '\0'
+	if (s[1] != '\0')
+		
 	{
 		_print_rev_recursion(&S[1]);
 	_putchar(s[0]);
