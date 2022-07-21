d3f7e8dcd8f5c9f4d0da2c4ae586aa484c5766b1
sachikosenpai <popoolafeyisayo5@gmail.com>
2022-07-21 05:04:51 -0700

    recursion

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -2,15 +2,15 @@
 
 /**
  * _print_rev_recursion - prints a string in reverse
- * @S: string to be printed
+ * @s: string to be printed
  *
  */
 void _print_rev_recursion(char *s)
 {
 	if (s[0] == '\0')
-		return ;
+		return;
 	if (s[1] != '\0')
-		
+
 	{
 		_print_rev_recursion(&s[1]);
 	_putchar(s[0]);
