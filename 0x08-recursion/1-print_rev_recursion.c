cf38d4266a218b9095532cbfc7551bd69c5fba10
sachikosenpai <popoolafeyisayo5@gmail.com>
2022-07-21 05:02:00 -0700

    recurson

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -8,11 +8,11 @@
 void _print_rev_recursion(char *s)
 {
 	if (s[0] == '\0')
-		return (0);
+		return ;
 	if (s[1] != '\0')
 		
 	{
-		_print_rev_recursion(&S[1]);
+		_print_rev_recursion(&s[1]);
 	_putchar(s[0]);
 	}
 
