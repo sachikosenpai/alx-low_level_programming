3efa1b4eca7382462ccb5a30d87cf0d0215f4eb6
sachikosenpai <popoolafeyisayo5@gmail.com>
2022-07-21 04:00:50 -0700

    Recursion

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * _puts_recursion - function that prints a string along with a new line
+ * @s: character
+ *
+ */
+
+void _puts_recursion(char *s)
+{
+	_puts_recursion(%s, char*s);
+
+		_putchar('\n');
+}
