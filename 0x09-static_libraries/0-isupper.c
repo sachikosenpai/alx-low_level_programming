fac6f8cb0d8bfe0f24cc51c5bdabc2e9de4a87d1
sachikosenpai <popoolafeyisayo5@gmail.com>
2022-07-22 17:52:40 -0700

    static

---
+++ 0x09-static_libraries/0-isupper.c
@@ -0,0 +1,20 @@
+#Inlude "main.h"
+
+/**
+ * _isupper - checks for uppercase character
+ * @c: character to be checked 
+ *
+ * Return: 1 if c is upper case otherwise 0
+ *
+ */
+
+
+int _isupper(int c)
+{
+
+	if (c >= 'A' && c <= 'Z')
+		
+		return (1);
+	else
+		return (0);
+}
