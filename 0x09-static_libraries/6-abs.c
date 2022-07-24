b29fa5dcb9ec4f47997a80be64c467493acbdac3
sachikosenpai <popoolafeyisayo5@gmail.com>
2022-07-24 12:20:39 -0700

    static library

---
+++ 0x09-static_libraries/6-abs.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _abs - computes the absolutes value of an integer
+ * @n: the integer to be computed
+ *
+ * Return: The absolute value of the integer
+ */
+
+int _abs(int n)
+{
+	if (n >= 0)
+		return (n);
+	else
+		return (-n);
+}
