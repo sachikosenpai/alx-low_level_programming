19ee7e9fefe577d6148e01f1c2b9983dd9555e38
sachikosenpai <popoolafeyisayo5@gmail.com>
2022-07-26 05:26:15 -0700

    arguments

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - prints the program name
+ * @argc: counts arguement
+ * @argv: value of the argument
+ * Return: 0 on success
+ */
+
+int main(int argc, char *argv[])
+{
+	while (argc--)
+		printf("%s\n", argv++);
+	return (0);
+}
