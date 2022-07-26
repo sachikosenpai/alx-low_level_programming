f7cebe635e484a862e40634ce98ad3a90cb819f5
sachikosenpai <popoolafeyisayo5@gmail.com>
2022-07-26 05:01:39 -0700

    name

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,5 +1,5 @@
 #include "main.h"
-
+#include <stdio.h>
 /**
  * main - entry point
  * @argv[]: array of pointer
@@ -8,8 +8,9 @@
  * Return: 0
  */
 
-int main (int __attribute__((__unused__)) argc, char *argv[])
+int main (int argc, char *argv[])
 {
-	printf("%s\n", argv[0]);
-		return (0);
+	while (argc--)
+		printf("%s\n", *argv++);
+	return (0);
 }
