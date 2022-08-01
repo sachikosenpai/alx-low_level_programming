50391e91af3100c970937db4d0074ee28c2a10cc
sachikosenpai <popoolafeyisayo5@gmail.com>
2022-08-01 06:42:51 -0700

    dog

--- 0x0E-structures_typedef/5-free_dog.c
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -11,8 +11,9 @@ void free_dog(dog_t *d)
 {
 	if (d == NULL)
 		return;
-
-		free(d->owner);
+	if (d->name != NULL)
 		free(d->name);
-		free(d);
+	if (d->owner != NULL)
+		free(d->owner);
+	free(d);
 }
