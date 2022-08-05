17884e5edd0c10977417e41a582c21ea2a30952b
sachikosenpai <popoolafeyisayo5@gmail.com>
2022-08-05 02:59:19 -0700

    print numbers

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -3,7 +3,7 @@
 #include <stdarg.h>
 
 /**
- * print_numbers - function to print numbers 
+ * print_numbers - function to print numbers
  * @separator: string to be printed among numbers
  * @n: number of integers passed to the function
  * Return: NULL if separator doesnt print
@@ -19,7 +19,7 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 	for (i = 0; i < n; i++)
 	{
 		printf("%i", va_arg(ap, int));
-		if (separator != NULL && i < n -1)
+		if (separator != NULL && i < n - 1)
 			printf("%s", separator);
 	}
 	printf("\n");
