a443f905c87ad3799d4dc4caaf825fbf24d8fdc5
sachikosenpai <popoolafeyisayo5@gmail.com>
2022-08-12 07:29:28 -0700

    asm

---
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -0,0 +1,18 @@
+
+
+section .data
+msg db "Hello, Holberton",10
+
+section .text
+global_start
+
+_start:
+	mov rax, 1
+	mov rdi, 1
+	mov rsi, msg
+	mov rdx, 13
+	syscall
+
+	mov rax, 60
+	mov rdi, 0
+	syscall
