37d6e48e7ea59d51efb6134d3f58791dd9518ab2
sachikosenpai <popoolafeyisayo5@gmail.com>
2022-08-12 04:39:25 -0700

    lis_list

---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+#include <stddef.h>
+
+/**
+ * list_len - a function that returns the number of
+ * elements in a linked list_t list.
+ * @h: pointer to the linked list
+ * Return: the number of elements in a linked list_t list
+ */
+
+size_t list_len(const list_t *h)
+{
+	size_t elem = 0;
+
+	while (h)
+	{
+		elem = 0;
+		h = h->next;
+	}
+
+}
--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -12,7 +12,7 @@
  * @next: points to the next node
  *
  * Description: singly listed note for this repository
- *
+ ingly_linked_lists
  */
 
 typedef struct list_s
@@ -24,6 +24,7 @@ typedef struct list_s
 
 
 size_t print_list(const list_t *h);
-
+size_t list_len(const list_t *h);
+int _putchar(char c);
 
 #endif /*LISTS_H*/
