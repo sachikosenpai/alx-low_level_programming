7a6f56d392a37e922d431eb01842e8cb8236f8b0
sachikosenpai <popoolafeyisayo5@gmail.com>
2022-08-12 05:14:37 -0700

    print_list

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -13,14 +13,14 @@ size_t print_list(const list_t *h)
 {
 	size_t nodes = 0;
 
-	while (h)
+	while (h != NULL)
 	{
 		if (h->str == NULL)
 			printf("[0], (nil)\n");
 		else
 			printf("[%d] %s\n", h->leng, h->str);
-		nodes++;
 		h = h->next;
+		nodes++;
 	}
 
 	return (nodes);
